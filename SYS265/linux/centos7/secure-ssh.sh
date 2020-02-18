f42cfa63a4e115ca6a14b4d4a26959f00b464c66
giangpham-cncs <giang.pham@mymail.champlain.edu>
2020-02-18 15:13:34 -0500

    web01

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author giangpham-cncs
+#creates a new ssh  user using $1 paramter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "all code here"
